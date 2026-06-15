342c1c0d2b69990e35446fa3c1894a242cbbf2bd
Girma N <swmrxy4@gmail.com>
2026-06-15 20:43:09 +0300

    fix: successfully typed logger config callbacks

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import pinoHttp from "pino-http";
@@ -11,14 +11,14 @@ app.use(
   (pinoHttp as any)({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
